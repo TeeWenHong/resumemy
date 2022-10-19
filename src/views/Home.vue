9b48a663faafc058daa8d6319a99f873ad75282b
TeeWenHong <61152364+TeeWenHong@users.noreply.github.com>
2022-10-19 12:50:08 +0800

    测试上传

---
+++ src/views/Home.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="home">
+    <el-form ref="formRef" :rules="rules" :model="form" label-width="40px" class="input">
+    <h1>你好老板！Hello Boss</h1>
+
+      <el-form-item  prop="name" class="username">
+        贵公司名称：<el-input v-model="form.name" class="laber" prefix-icon="el-icon-name" ></el-input>
+      </el-form-item>
+
+      <el-form-item  prop="email" class="username">
+        邮箱：<el-input v-model="form.email" class="laber" prefix-icon="el-icon-user" ></el-input>
+      </el-form-item>
+      
+      <el-form-item  prop="phone" class="username">
+        电话：<el-input v-model="form.nember" class="laber" prefix-icon="el-icon-user" ></el-input>
+      </el-form-item>
+
+      <div class="username2">岗位:
+        <el-select style=" width:410px" v-model="value" placeholder="请选择" prop="j" >
+        <el-option
+        v-for="item in form.options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+        </el-option> 
+      </el-select>
+      </div>
+      
+
+      <!-- 提交按钮 -->
+      <el-form-item class="button">
+        <el-button type="primary" @click="loginCheck()">提交看简历</el-button>
+      </el-form-item>
+
+      <!-- 跳转按钮 -->
+      <el-form-item class="button2">
+        <el-button type="primary" @click="resumeCheck()">先看看简历</el-button>
+      </el-form-item>
+      
+
+    </el-form>
+ 
+    
+    
+  </div> 
+</template>
+
+<script>
+export default {
+  // el:"app",
+  data() {
+    return {
+     form:{
+       name:"hgg",
+       email:"@gmail.com",
+       nember:"01",
+       job:"",
+       options: [{
+          value: '1',
+          label: '前端web'
+        }, {
+          value: '2',
+          label: '前端android',
+        }, {
+          value: '3',
+          label: '后端'
+        }, {
+          value: '4',
+          label: '数据库'
+        }]
+     },
+     rules:{
+      name:[
+        {required:true,message:'请输入公司名称',trigger:'blur'},
+        {min:3,max:20,message:'长度在3到20个字',trigger:'blur'}
+      ],
+      email:[
+        {required:true,message:'请输入联系邮箱',trigger:'blur'},
+        {min:3,max:20,message:'长度在3到20个字',trigger:'blur'}
+      ],
+      phone:[
+        {required:true,message:'请输入联系号码',trigger:'blur'},
+        {min:6,max:12,message:'长度在6到12个字',trigger:'blur'}
+      ]
+     }
+      // options: [{
+      //     value: '选项1',
+      //     label: '前端web'
+      //   }, {
+      //     value: '选项2',
+      //     label: '前端android',
+      //   }, {
+      //     value: '选项3',
+      //     label: '后端'
+      //   }, { 
+      //     value: '选项4',
+      //     label: '数据库'
+      //   }]
+    }
+  },
+  methods:{
+    resumeCheck(){
+    this.$router.push("./resume")
+    },
+    loginCheck(){
+      this.$refs.formRef.resetFileds();
+    }
+  },
+  // el:'',
+  // data() {
+  //   return{
+
+  //   }
+  // }
+  
+}  
+</script>
+
+<style>
+  html, body{
+        margin: 0;
+        padding: 0;
+    }
+
+.button{
+  position:absolute;
+  top: 85%;
+  left: 15%;
+}
+
+.button2{
+  position:absolute;
+  top: 85%;
+  left: 50%;
+}
+
+.home{
+  background-color: aqua;
+  max-width: 1500px;
+  min-width: 600px;
+  min-height: 770px;
+  max-height: 1000px;
+  background-image: url(./img/002.jpg);
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+
+.input{
+  background-color: antiquewhite;
+  max-height: 510px;
+  max-width: 500px;
+  min-width: 510px;
+  min-height: 500px;
+  transform: translate(-40%,-50%);
+  position:absolute;
+  left:50%;
+  top:50%;
+  border-radius: 15px;
+}
+
+.username{
+  /* background-color: aqua; */
+  height: 60px;
+  width: 450px;
+}
+.username2{
+  /* background-color: aqua; */
+  height: 60px;
+  width: 400px;
+  position:absolute;
+  left: 40px;
+  top:340px;
+}
+
+</style>
+
---
+++ src/views/resume.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="resume">
+    <div class="body_info">
+      <div class="main_info" >
+        <h1>我的简历</h1>
+          <div class="img_class"><img class="my_img" src="./img/img.jpg" ></div>
+          <div class="main">
+             <p>姓名：郑文鸿（TeeWenHong）</p>
+             <p>年龄：25</p>
+             <p>性别：男</p>
+             <p>学历：武汉大学（软件工程）</p>
+             <p>
+              ---------------------------------------------               
+             </p>
+          </div>
+          <div class="main2">
+              <h2>联系方式</h2>
+              <p>电话号码：016-920-3983</p>
+              <p>邮箱：gqinjia55@gmail.com</p>
+              <h5>注：如有任何问题请使用whatapp联系，电话接通不到谢谢您</h5>
+              <p>
+                  ---------------------------------------------
+              </p>
+           </div>
+          <div class="main3">
+              <h2>个人能力</h2>
+              <h3>前端:</h3>
+              <div class="todo1"><p class="todo1_1">Vue2.0<img src="./img/003.png" width="50px" /></p>
+              <p class="todo1_1">elementUI<img src="./img/004.png" width="50px"/></p>
+              </div>
+              <h3>后端:(JAVA)</h3>
+              <div>
+                <div class="todo1">
+                <p class="todo1_1">SpringBoot <img src="./img/005.jpeg" width="50px"/></p>
+                <p class="todo1_2">MyBatis <img src="./img/006.png" width="50px"/></p>
+                </div>
+                
+              </div>
+              <h3>数据库:</h3>
+
+              <div class="todo1">
+                <p class="todo1_1">MySQL <img src="./img/007.png" width="50px"/></p>
+              </div>
+              
+          </div>
+         </div>
+        <div class="resume_info">
+          <h2>{{text}}</h2>
+          <h2>{{text2}}</h2>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text:"hjadgcjhagdcad",
+      text2:"asdfadsfadfadsfadyuqiwerkzbvcxlvkgwericbvqogufdsbvaergquierwtbszf"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+.todo1{
+  /* background-color: rgb(175, 122, 51); */
+  width: 240px;
+  height: 85px;
+  display: flex;
+  
+}
+.todo1_1{
+    /* background-color: aqua; */
+    width: 100px;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    
+}
+
+.todo1_2{
+    /* background-color: aqua; */
+    width: 100px;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    position:absolute;
+    left: 145px;
+}
+
+
+.resume{
+  background-color: aqua;
+  width: 1500px;
+  height: 1500px;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+.main3{
+    background-color: rgb(0, 0, 0);
+    margin-left: 5px;
+    margin-top: 100px;
+    height: 350px;
+    width: 240px;
+}
+
+.resume_info{
+    background-color: rgb(255, 255, 255);
+    width: 510px;
+    height: 1250px;
+    margin-left: 2%;
+    border-radius: 15px;
+    word-wrap: break-word;
+    word-break: normal;
+    text-indent: 30px;
+}
+
+.body_main{
+    background-color: rgb(65, 208, 255);
+    
+}
+
+.body_info{
+    background-color: rgb(209, 255, 255);
+    width: 800px;
+    height: 1300px;
+    border-radius: 15px;
+    display: flex;
+    position:absolute;
+    align-items: center;
+    margin-left: 20%;
+    top: 10%;
+    
+}
+
+.main_info{
+    background-color: rgb(0, 0, 0);
+    width: 250px;
+    height: 1250px;
+    border-radius: 10px;
+    color: white;
+    margin-left: 10px;
+}
+
+.my_img{
+    background-color: bisque;
+    border-radius:100%; 
+    width:100px; 
+    height:100px;
+    overflow: hidden;
+    margin-left: 25%;
+    
+
+}
+
+.img_class{
+    display: flex;
+    width: 250px;
+    height: 100px;
+    background-color: rgb(0, 0, 0);
+}
+
+.main{
+    height: 150px;
+    width: 240px;
+    margin-left: 5px;
+    background-color: rgb(0, 0, 0);
+}
+
+.main2{
+    margin-left: 5px;
+    height: 150px;
+    width: 240px;
+
+}
+
+
+</style>
